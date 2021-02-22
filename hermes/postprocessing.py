f17a15210016df976d0ba3753170d5901135e028
Jos van 't Hof <josvth@gmail.com>
2021-02-22 09:23:03 -0800

    Cleaned-up post-processing.

--- hermes/postprocessing.py
+++ hermes/postprocessing.py
@@ -5,11 +5,9 @@ from tqdm import tqdm
 from astropy import units as u, time
 from astropy.coordinates import GCRS, CartesianRepresentation, ITRS, SphericalRepresentation
 
-# Functions to quickly grab state-vectors as numpy arrays
 from hermes.geometry import find_quaternions, rotate_vectors, bulk_rotate
-from hermes.util import row_cross
 
-
+# Functions to quickly grab state-vectors as numpy arrays
 def get_r_a(instances_df):
     return np.array([instances_df['r_a_x'].values, instances_df['r_a_y'].values, instances_df['r_a_z'].values]).T
 
@@ -65,7 +63,7 @@ def add_range(instances_df):
     return instances_df
 
 
-def add_sff_2(instances_df):
+def add_sff(instances_df):
     r_a, v_a = get_rv_a(instances_df)
     r_b = get_r_b(instances_df)
 
@@ -91,59 +89,6 @@ def add_sff_2(instances_df):
     instances_df['r_ab_sff_z'] = r_ab_sff_z
 
     return instances_df
-
-
-def add_sff(instances_df):
-    import quaternionic as qn
-
-    # State vector of satellite a
-    r_a, v_a = get_rv_a(instances_df)
-    r_a = r_a[0:10, :]
-    v_a = v_a[0:10, :]
-
-    N = r_a.shape[0]
-
-    # ECIF frame
-    x_ecif = np.tile(np.array([1, 0, 0]), (N, 1))
-    y_ecif = np.tile(np.array([0, 1, 0]), (N, 1))
-    z_ecif = np.tile(np.array([0, 0, 1]), (N, 1))
-
-    # Find the quaternion that aligns x
-    q_x = qn.array(find_quaternions(x_sff_in_ecif, x_ecif))
-    x_test = rotate_vectors(x_sff_in_ecif, q_x)
-
-    # Rotate other axes to follow
-    y_int = rotate_vectors(y_sff_in_ecif, q_x)
-    z_int = rotate_vectors(z_sff_in_ecif, q_x)
-
-    # Find the quaternion that aligns z
-    q_z = qn.array(find_quaternions(z_int, z_ecif))
-    z_test = rotate_vectors(z_int, q_z)
-
-    y_test1 = rotate_vectors(y_int, q_z)
-    y_test2 = rotate_vectors(y_sff_in_ecif, q_z * q_x)
-    y_test3 = rotate_vectors(y_sff_in_ecif, q_z)
-
-    x_test2 = rotate_vectors(x_sff_in_ecif, q_z * q_x)
-    z_test2 = rotate_vectors(z_sff_in_ecif, q_z * q_x)
-    pass
-
-    # # Find quaternion that rotates x to the right place
-    # q_x = qn.array(find_quaternions(x_ecif, x_sff_in_ecif))
-    #
-    # # Then rotate the z to this intermediate frame
-    # z_int = rotate_vectors(z_ecif, q_x)
-    #
-    # # Then find the vector that rotate this intermediate  that rotates z to the right place
-    # q_z = qn.array(find_quaternions(z_int, z_sff_in_ecif))
-    #
-    # q_ecif_sff = q_x * q_z
-    #
-    # # Test
-    # x_test = rotate_vectors(x_ecif, q_ecif_sff)
-
-    return instances_df
-
 
 ### Generate single line per pass dataframes
 def generate_pass_df(instance_df):
@@ -190,156 +135,3 @@ def generate_gap_df(instances_df, step_duration=1.0, tof_start=0.0, tof_end=None
     gaps_df = pd.DataFrame(gaps_dict, index=list(range(len(start_tofs))) if isinstance(start_tofs, np.ndarray) else [0])
     gaps_df.index.name = 'g'
     return gaps_df
-
-
-### OLD DO NOT USE
-def instances_to_passes_fast(instances_df, step_duration=1.0):
-    # Sort in chronological order (if not already)
-    # instances_df = instances_df.sort_values(by= 'tof')
-
-    # Combine xyz positions
-    r_a = np.vstack(
-        (instances_df.xyz_a_x.to_numpy().T, instances_df.xyz_a_y.to_numpy().T, instances_df.xyz_a_z.to_numpy().T)).T
-    r_b = np.vstack(
-        (instances_df.xyz_b_x.to_numpy().T, instances_df.xyz_b_y.to_numpy().T, instances_df.xyz_b_z.to_numpy().T)).T
-
-    instances_df = instances_df.assign(r_a=pd.Series(np.vsplit(r_a, len(r_a))))
-    instances_df = instances_df.assign(r_b=pd.Series(np.vsplit(r_b, len(r_b))))
-    instances_df = instances_df.drop(columns=['xyz_a_x', 'xyz_a_y', 'xyz_a_z', 'xyz_b_x', 'xyz_b_y', 'xyz_b_z'])
-
-    # Group by strand
-    grouped_df = instances_df.groupby('StrandName')
-
-    pp = []
-
-    pass_id = 0
-
-    with tqdm(total=len(grouped_df), desc='Group') as group_bar:
-        for name, group in grouped_df:
-
-            # Calculate delta time between instances
-            group = group.assign(
-                delta_tof=group['tof'] - np.concatenate(([group['tof'].iloc[0]], group['tof'].iloc[0:-1].values)))
-
-            i_l = group.index[0]
-            for i in group.index[group['delta_tof'] > 2 * step_duration]:
-
-                # Select instances between indexes
-                pass_instances = group.loc[i_l:i - 1]
-
-                if pass_instances.tof.values[1] == pass_instances.tof.values[0]:
-                    pass
-
-                # Create pass df
-                p = {"StartTof": pass_instances.iloc[0].tof,
-                     "StopTof": pass_instances.iloc[-1].tof,
-                     "ttof": [pass_instances.tof.values],
-                     "rr_a": [np.concatenate(pass_instances.r_a.values)],
-                     "rr_b": [np.concatenate(pass_instances.r_b.values)],
-                     "rrange": [np.linalg.norm(
-                         np.concatenate(pass_instances.r_b.values) - np.concatenate(pass_instances.r_a.values),
-                         axis=1)],
-                     'StrandName': pass_instances.iloc[0].StrandName}
-
-                # Create pass df and add to list
-                pass_df = pd.DataFrame.from_dict(p)
-                pp.append(pass_df)
-
-                # # plot pass
-                # import matplotlib.pyplot as plt
-                # fig, ax = plt.subplots()
-                # ax.plot(pass_df.ttof[0], pass_df.rrange[0])
-                # fig.show()
-
-                i_l = i
-
-            group_bar.update(1)
-
-        passes_df = pd.concat(pp, ignore_index=True)
-        passes_df['Duration'] = passes_df['StopTof'] - passes_df['StartTof']
-        # return passes_df
-        return passes_df.sort_values(by='StartTof').reset_index(drop=True)
-
-
-def instances_to_passes(instances_df, step_duration=1.0):
-    # Sort in chronological order (if not already)
-    instances_df = instances_df.sort_values(by='tof')
-
-    # Combine xyz positions
-    r_a = np.vstack(
-        (instances_df.xyz_a_x.to_numpy().T, instances_df.xyz_a_y.to_numpy().T, instances_df.xyz_a_z.to_numpy().T)).T
-    r_b = np.vstack(
-        (instances_df.xyz_b_x.to_numpy().T, instances_df.xyz_b_y.to_numpy().T, instances_df.xyz_b_z.to_numpy().T)).T
-
-    instances_df = instances_df.assign(r_a=pd.Series(np.vsplit(r_a, len(r_a))))
-    instances_df = instances_df.assign(r_b=pd.Series(np.vsplit(r_b, len(r_b))))
-    instances_df = instances_df.drop(columns=['xyz_a_x', 'xyz_a_y', 'xyz_a_z', 'xyz_b_x', 'xyz_b_y', 'xyz_b_z'])
-
-    # Group by strand
-    grouped_df = instances_df.groupby('StrandName')
-
-    pp = []
-    passes_df = pd.DataFrame()
-
-    pass_id = 0
-
-    with tqdm(total=len(grouped_df), desc='Group') as group_bar:
-        for name, group in grouped_df:
-
-            p = {"ID": pass_id,
-                 "StartTof": group.iloc[0].tof,
-                 "StopTof": group.iloc[0].tof,
-                 "ttof": [group.iloc[0].tof],
-                 "rr_a": [group.iloc[0].r_a],
-                 "rr_b": [group.iloc[0].r_b],
-                 'StrandName': group.iloc[0].StrandName}
-
-            for index, instance in group.iterrows():
-
-                last_tof = p["StopTof"]  # last found tof in this pass
-                instance_tof = instance.tof  # tof of this new instance
-
-                if instance_tof - last_tof <= 1.5 * step_duration:  # check if this step is part of the previous period
-                    # if so the stop time is extended
-                    p["StopTof"] = instance_tof
-
-                    # and the tof and xyz coordinates are added to the lists
-                    p["ttof"].append(instance_tof)
-                    p["rr_a"].append(instance.r_a)
-                    p["rr_b"].append(instance.r_b)
-
-                else:
-                    # if not then a pass was finished, add it to the passes_df and continue
-                    p["ttof"] = [np.hstack(p["ttof"])]
-                    p["rr_a"] = [np.vstack(p["rr_a"])]
-                    p["rr_b"] = [np.vstack(p["rr_b"])]
-
-                    pass_df = pd.DataFrame.from_dict(p)
-                    pass_df.set_index("ID")
-                    pp.append(pass_df)
-
-                    pass_id = pass_id + 1  # increment pass id
-
-                    # initialize the next pass df
-                    p = {"ID": pass_id,
-                         "StartTof": instance.tof,
-                         "StopTof": instance.tof,
-                         "ttof": [instance.tof],
-                         "rr_a": [instance.r_a],
-                         "rr_b": [instance.r_b],
-                         'StrandName': instance.StrandName}
-
-            group_bar.update(1)
-
-    # Finalize the last row
-    pass_df = pd.DataFrame.from_dict(p)
-    pass_df.set_index("ID")
-    pp.append(pass_df)
-
-    # Create dataframe of passes
-    passes_df = pd.concat(pp)
-    passes_df.set_index("ID")
-    # Calculate total duration of passes
-    passes_df['Duration'] = passes_df['StopTof'] - passes_df['StartTof']
-
-    return passes_df
